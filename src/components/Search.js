2ed7b47b14ec07a3ce89906d22e971e7b9cc9d69
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-11-03 20:50:19 +0530

    Implemented debouncing.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,29 +1,75 @@
-import React,{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
 
 
-    const [term,setTerm]= useState('');
+    const [term, setTerm] = useState('programming');
+
+    const [results, setResults] = useState([]);
 
 
-    useEffect(()=>{
-        
-        const search= async ()=>{
-            await axios.get('https://en.wikipedia.org/w/api.php',{
-                params:{
+    useEffect(() => {
+
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
                     action: 'query',
                     list: 'search',
                     origin: '*',
-                    format:'json',
+                    format: 'json',
                     srsearch: term
                 }
             });
+
+            setResults(data.query.search);
         };
 
-        search();
+        if(term && !results.length){
+            search();
+        }
+        else{
+            let timeoutId = setTimeout(() => {
+                if (term) {
+                    search();
+                }
+            }, 500
+            );
+    
+    
+            return () => {
+                clearTimeout(timeoutId);
+            };
+        }
 
-    },[term]);
+ 
+
+
+    }, [term]);
+
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div className="item" key={result.pageid}>
+                <div className="right floated content">
+                    <a className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}>
+                    </span>
+
+                </div>
+
+            </div>
+        );
+    });
 
 
 
@@ -32,11 +78,14 @@ const Search = () => {
             <div className="ui form">
                 <div className="field">
                     <label> Enter Search Term</label>
-                    <input value={term} onChange={e => setTerm(e.target.value)} className="input"/>
+                    <input value={term} onChange={e => setTerm(e.target.value)} className="input" />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );
 };
 
-export default Search; 
+export default Search;
